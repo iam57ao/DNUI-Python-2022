ae4f845e9bff2e9406a6e937a6b25974e1bd01f7
iam57ao <me@iam57.com>
2022-11-17 19:21:24 +0800

    简化了一些算法

--- 题目集/81-100/7-84.py
+++ 题目集/81-100/7-84.py
@@ -1,7 +1,6 @@
 num = int(input())
-num_str = str(num)
 for i in range(num):
     if i == 0 or i == num - 1:
-        print(num_str * num)
+        print(num * str(num))
     else:
-        print(num_str + " " * (num - 2) + num_str)
+        print(f"{num}{' ' * (num - 2)}{num}")
