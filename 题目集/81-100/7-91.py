57af87b41f23d6f01552b40a55eaf37d84de16b3
iam57ao <me@iam57.com>
2022-11-27 11:05:24 +0800

    更新题目

--- 题目集/81-100/7-91.py
+++ 题目集/81-100/7-91.py
@@ -1,16 +1,5 @@
-num_input = input().split()
-m, n = sorted(list(map(int, num_input)))
-
-num_list = []
-i, j = 1, 1
-for element in range(1, m + 1):
-    if m % element == 0 and n % element == 0:
-        num_list.append(element)
-        m, n = m // element, n // element
-num_list.extend([m, n])
-for element in num_list[:-2]:
-    i *= element
-j = i
-for element in num_list[-2:]:
-    j *= element
-print(i, j)
+a, b = map(int, input().split())
+value = a * b
+while a % b != 0:
+    a, b = b, a % b
+print(b, value // b)
