f94fe78bec64328320f3c4a953461982d7da4de5
iam57ao <me@iam57.com>
2022-12-02 10:11:27 +0800

    更新题目

--- 题目集/121-133/7-126.py
+++ 题目集/121-133/7-126.py
@@ -2,7 +2,7 @@ for count in range(int(input())):
     n = int(input())
     out_list = [[0 for i in range(n)] for j in range(n)]
     if n % 2 != 0:  # 判断是否为奇数
-        out_list[n // 2][n // 2] = 1  # 如果是奇数，中间放入n的平方
+        out_list[n // 2][n // 2] = 1  # 如果是奇数，中间放入1
     for level in range(n // 2):  # 层数循环
         for right in range(n - 1 - 2 * level):  # 同层向右
             out_list[level][right + level] = (n - 2 * level) ** 2 - right
